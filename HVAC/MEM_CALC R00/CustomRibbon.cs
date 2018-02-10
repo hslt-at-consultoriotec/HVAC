71eb93d8eb9036430b747f488d45f6636cd50fc3
hslt-at-accenture <hector.s.lopez.tapia@accenture.com>
2018-02-10 00:27:52 -0600

    Added document custom ribbon

---
+++ HVAC/MEM_CALC R00/CustomRibbon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace MEM_CALC_R00
+{
+    public partial class CustomRibbon
+    {
+        private void CustomRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+    }
+}
